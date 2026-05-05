2ef5fa5c10d9f798bf771c0750b60527ea3d7a77
Thomas Quan <dev.thomasquan@gmail.com>
2026-05-04 22:41:07 -0400

    add my work lsp and formatter and linter

--- init.lua
+++ init.lua
@@ -611,7 +611,6 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
         gopls = {
           settings = {
             gopls = {
@@ -622,26 +621,24 @@ require('lazy').setup({
             },
           },
         },
+
         pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
+
+        -- Python type checker (optional alongside pyright)
+        -- mypy is NOT an LSP, so it does NOT go here
+
         ts_ls = {},
+
+        tailwindcss = {},
+
+        shopify_theme_ls = {},
+
         lua_ls = {
-          -- cmd = { ... },
-          -- filetypes = { ... },
-          -- capabilities = {},
           settings = {
             Lua = {
               completion = {
                 callSnippet = 'Replace',
               },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
             },
           },
         },
@@ -659,7 +656,24 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
+        'stylua', -- Lua formatter
+
+        -- JS / TS
+        'prettierd',
+        'prettier',
+        'ast-grep',
+
+        -- Python
+        'black',
+        'isort',
+        'mypy',
+
+        -- Go
+        'gofumpt',
+        'goimports',
+
+        -- (optional but useful)
+        'shfmt',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
